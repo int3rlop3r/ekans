11d066c142b799174173f918c56b690f7f2f9868
Jonathan Fernandes <int3rlop3r@yahoo.in>
2022-01-31 20:06:07 +0530

    Buffer the output and write 'A' at location (10, 50)

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"golang.org/x/term"
 )
@@ -21,8 +23,10 @@ type Inst struct {
 
 func clearScreen(mode int) {
 	instrs := []Inst{
-		{mBoth, "\x1b[2J"},    // clear the screen
-		{mBoth, "\x1b[H"},     // CUP - get the cursor UP (top left)
+		{mBoth, "\x1b[2J"}, // clear the screen
+		{mBoth, "\x1b[H"},  // CUP - get the cursor UP (top left)
+		//{mStart, "\x1b[2J"},   // clear the screen
+		//{mStart, "\x1b[H"},    // CUP - get the cursor UP (top left)
 		{mStart, "\x1b[?25l"}, // hide the cursor
 		{mEnd, "\x1b[?25h"},   // display the cursor
 	}
@@ -50,18 +54,7 @@ func init() {
 	state, initError = term.MakeRaw(stdinFd)
 }
 
-func main() {
-	if initError != nil {
-		fmt.Fprintln(os.Stdout, "err:", initError)
-		return
-	}
-	defer shutDown(stdinFd, state)
-
-	// we'll use this later to create a buffer so that we can
-	// buffer changes and then write them to the screen
-	//var term unix.Termios
-	//c, r, err := term.GetSize(stdinFd)
-
+func processKeyPress() {
 OUT:
 	for {
 		buf := make([]byte, 1)
@@ -69,7 +62,7 @@ OUT:
 		switch err {
 		case nil: // no error, process input
 			b := buf[0]
-			fmt.Print(b)
+			fmt.Print(b) // instead of printing we'll have to return events
 			if b == 4 || b == 3 {
 				fmt.Print("qutting!\r\n")
 				break OUT
@@ -81,4 +74,41 @@ OUT:
 			break
 		}
 	}
+}
+
+func makeBuff(r, c int) [][]byte {
+	buf := make([][]byte, r-1)
+	for i := range buf {
+		buf[i] = make([]byte, c)
+		for j := range buf[i] {
+			buf[i][j] = ' ' // fill the buffer with spaces
+		}
+	}
+	return buf
+}
+
+func display(buf [][]byte) {
+	fmt.Printf("%s\r\n", bytes.Join(buf, []byte("\r\n")))
+}
+
+func main() {
+	if initError != nil {
+		fmt.Fprintln(os.Stderr, "err:", initError)
+		return
+	}
+	defer shutDown(stdinFd, state)
+
+	// we'll use this later to create a buffer so that we can
+	// buffer changes and then write them to the screen
+	//var term unix.Termios
+	c, r, err := term.GetSize(stdinFd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't get screen size, err:", err)
+		return
+	}
+	fmt.Printf("rows: %d, cols: %d\r\n", r, c)
+	buf := makeBuff(r, c)
+	buf[10][50] = 'A'
+	display(buf)
+	time.Sleep(20 * time.Second)
 }
